dbddf97a9ce6a91c4c6cc56f6df24dacc3a81b78
Nitsuhe <mesheshanitsuh@gmail.com>
2022-10-13 05:26:15 -0700

    C-programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - First C
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char l;
+	int m;
+	long int p;
+	long long int o;
+	float p;
+
+	printf("Size of a char:%d byte(s)\n", sizeof(l));
+	printf("Size of a int:%d byte(s)\n", sizeof(m));
+	printf("Size of a long int:%d byte(s)\n", sizeof(p));
+	printf("Size of a long long int:%d byte(s)\n", sizeof(o));
+	printf("Size of a float:%d byte(S)\n", sizeof(p));
+	return (0);
+
