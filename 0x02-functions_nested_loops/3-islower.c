38e15933f72b379b4b347718fa338f11a298af0b
Nitsuhe <mesheshanitsuh@gmail.com>
2022-10-18 05:52:49 -0700

    C function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - alphabet
+ *
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * abs - value of integer
+ *
+ * Return: Always 0
+ */
+int _abs(int m)
+{
+	if (m >= 0)
+		return (m);
+	else
+		return (-m);
+}
